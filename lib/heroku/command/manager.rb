446562db0643e89935ff75b77c37bf9879512f0e
John Simone <john@heroku.com>
2012-11-03 18:39:02 -0700

    update transfer with new flow

--- lib/heroku/command/manager.rb
+++ lib/heroku/command/manager.rb
@@ -19,8 +19,6 @@ class Heroku::Command::Manager < Heroku::Command::BaseWithApp
     display "heroku manager:add_contributor_to_app --org ORG_NAME --user USER_EMAIL [--app APP_NAME]"
     display "Heroku Teams Migration Commands:"
     display "heroku manager:team_to_org --team TEAM_NAME --org ORG_NAME"
-    display "heroku manager:org_to_team --org ORG_NAME --team TEAM_NAME"
-
   end
 
   # manager:transfer (--to|--from) ORG_NAME [--app APP_NAME]
@@ -50,13 +48,27 @@ class Heroku::Command::Manager < Heroku::Command::BaseWithApp
 
     begin
       if to
+        #TODO org info call to check access
+        collaborators = api.get_collaborators(app).body
+        collaborators = filter_org_members(collaborators, to)
+        if !collaborators.empty?  #if there are no collaborators that aren't org members don't prompt the user
+          collabs_to_add = get_collabs_to_add(collaborators, prompt_for_collaborators(collaborators))
+          #TODO handle bad input
+          add_collabs_to_org(collabs_to_add, to)
+        end
         print_and_flush("Transferring #{app} to #{to}... ")
         response = RestClient.post("https://:#{api_key}@#{MANAGER_HOST}/v1/organization/#{to}/app", json_encode({ "app_name" => app }), :content_type => :json)
         if response.code == 201
-          print_and_flush(" done\n")
+          print_and_flush("done\n")
         else
           print_and_flush("failed\nAn error occurred: #{response.code}\n#{response}")
         end
+        #should be part of the manager api
+#        if !collabs_to_add.empty?
+#          print_and_flush("restoring access...")
+#          add_collabs_to_app(collabs_to_add, app, to)
+#          print_and_flush("transfer complete")
+#        end
       else
         print_and_flush("Transferring #{app} from #{from} to your personal account... ")
         response = RestClient.post("https://:#{api_key}@#{MANAGER_HOST}/v1/organization/#{from}/app/#{app}/transfer-out", "")
@@ -67,12 +79,82 @@ class Heroku::Command::Manager < Heroku::Command::BaseWithApp
         end
       end
     rescue => e
+      print_and_flush("failed\nAn error occurred: #{e.message}\n")
       if e.response
         errorText = json_decode(e.response.body)
         print_and_flush("failed\nAn error occurred: #{errorText["error_message"]}\n")
       else
         print_and_flush("failed\nAn error occurred: #{e.message}\n")
       end
+    end
+  end
+
+  def prompt_for_collaborators(collaborators)
+
+    display
+    message ||= "There are collaborators on this app that are not members of this org. \n"
+    message << "current collaborators:\n"
+    i = 1
+    collaborators.each { |collab|
+      message << "#{i}: #{collab["email"]}\n"
+      i = i+1
+    }
+    message << "Enter the number of any that you would like added to this org seperated by a comma.\n"
+    output_with_bang(message)
+    display
+    display "> ", false
+    ask.split(",").map{ |x| x.to_i}
+  end
+
+  def get_collabs_to_add(collaborators, indexes_to_add)
+    i = 0
+    collaborators.select { |collab|
+      i += 1
+      indexes_to_add.include?(i)
+    }
+  end
+
+  def add_collabs_to_org(collaborators_to_add, org)
+    collaborators_to_add.each { |collab_to_add|
+      add_user_to_org(collab_to_add["email"], org)
+    }
+  end
+
+  def add_collabs_to_app(collaborators_to_add, app, org)
+    collaborators_to_add.each { |collab_to_add|
+      give_access_to_member(collab_to_add["email"], app, org)
+    }
+  end
+
+  def give_access_to_member(user, app, org)
+    print_and_flush("Adding #{user} to #{app} in org... ")
+
+    response = RestClient.post("https://:#{api_key}@#{MANAGER_HOST}/v1/organization/#{org}/app/#{app}/developer", json_encode({ "email" => user }), :content_type => :json)
+
+    if response.code == 201
+      print_and_flush("done\n")
+    else
+      print_and_flush("failed\nAn error occurred: #{response.code}\n#{response}\n")
+    end
+  end
+
+  def filter_org_members(collaborators, org)
+      user_list = json_decode(RestClient.get("https://:#{api_key}@#{MANAGER_HOST}/v1/organization/#{org}/user")).map {
+        |u| u["email"]
+      }
+      collaborators.select { |collab|
+        !user_list.include?(collab["email"])
+      }
+  end
+
+  def add_user_to_org(user, org)
+    print_and_flush "adding #{user} to org..."
+    response = RestClient.post("https://:#{api_key}@#{MANAGER_HOST}/v1/organization/#{org}/user", json_encode({ "email" => user, "role" => "member" }), :content_type => :json)
+
+    if response.code == 201
+      print_and_flush(" done\n")
+    else
+      print_and_flush("failed\nAn error occurred: #{response.code}\n#{response}\n")
     end
   end
 
@@ -113,46 +195,6 @@ class Heroku::Command::Manager < Heroku::Command::BaseWithApp
         print_and_flush("failed\nAn error occurred: #{e.message}\n")
       end
     end
-  end
-
-  # manager:org_to_team --org ORG_NAME --team TEAM_NAME
-  #
-  # transfer all apps from an organization to a team
-  #
-  # -t, --team TEAM     # Transfer applications to this team
-  # -o, --org ORG       # Transfer applications from this org
-  #
-  def org_to_team
-    team = options[:team]
-    org = options[:org]
-
-    if team.nil?
-      raise Heroku::Command::CommandFailed, "No team specified.\nSpecify which team to transfer to with --team <team name>.\n"
-    end
-
-    if org.nil?
-      raise Heroku::Command::CommandFailed, "No organization specified.\nSpecify which organization to transfer from with --org <org name>.\n"
-    end
-
-    print_and_flush("Transferring apps from #{org} to #{team}... ")
-
-    begin
-      response = RestClient.post("https://:#{api_key}@#{MANAGER_HOST}/v1/organization/#{org}/migrate-to-team", json_encode({ "team" => team }), :content_type => :json)
-
-      if response.code == 200
-        print_and_flush(" done\n")
-      else
-        print_and_flush("failed\nAn error occurred: #{response.code}\n#{response}\n")
-      end
-    rescue => e
-      if e.response
-        errorText = json_decode(e.response.body)
-        print_and_flush("failed\nAn error occurred: #{errorText["error_message"]}\n")
-      else
-        print_and_flush("failed\nAn error occurred: #{e.message}\n")
-      end
-    end
-
   end
 
   # manager:add_user --org ORG_NAME --user USER_EMAIL --role ROLE
